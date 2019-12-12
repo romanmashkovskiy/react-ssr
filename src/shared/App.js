2e2a9f18cb2d71ffba50665d049b25257a44f1b0
php vsegda <phpvsegda@gmail.com>
2019-12-12 22:13:36 +0200

    finished

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import routes from './routes';
+import Navbar from './Navbar';
+import NoMatch from './NoMatch';
 
 class App extends Component {
     render() {
         return (
             <div>
-                Hello {this.props.data}
+                <Navbar/>
+                <Switch>
+                    { routes.map(({ path, exact, component: C, ...rest }) => (
+                        <Route
+                            key={ path }
+                            path={ path }
+                            exact={ exact }
+                            render={ (props) => (
+                                <C { ...props } { ...rest } />
+                            ) }
+                        />
+                    )) }
+                </Switch>
             </div>
         )
     }
 }
 
-export default App
+export default App;
